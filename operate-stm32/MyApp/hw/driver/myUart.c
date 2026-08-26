ab252b095d34ffff44740d56f17068ed419c4d50
Jaewoo Na <marajewoo987@gmail.com>
2026-08-26 17:16:02 +0900

    update mybt

--- operate-stm32/MyApp/hw/driver/myUart.c
+++ operate-stm32/MyApp/hw/driver/myUart.c
@@ -14,42 +14,50 @@ PUTCHAR_PROTOTYPE
   return ch;
 }
 
-#define RX_BUF_SIZE 128
-uint8_t rx_data;
-uint8_t rx_buf[RX_BUF_SIZE];
+// #define RX_BUF_SIZE 128
+// uint8_t rx_data;
+// uint8_t rx_buf[RX_BUF_SIZE];
 
 void uartInit(void){
   //HAL_UART_Receive_IT(&huart2, &rx_data, 1);
-  HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf, RX_BUF_SIZE);
+  bt_Reset();
+  // HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf, RX_BUF_SIZE);
 }
 
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 
-  if(huart->Instance==USART2){
-    if(rx_data=='a')
-      printf("Hello STM32 Cortex-M4 USART Polling!\r\n");
-    else
-      HAL_UART_Transmit(&huart2, rx_buf, RX_BUF_SIZE, 100);
+// void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 
-    HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf, RX_BUF_SIZE);
-  }
-}
-void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
-{
-  if(huart->Instance==USART2){
+//   if(huart->Instance==USART2){
+//     if(rx_data=='a')
+//       printf("Hello STM32 Cortex-M4 USART Polling!\r\n");
+//     else
+//       HAL_UART_Transmit(&huart2, rx_buf, RX_BUF_SIZE, 100);
 
-    HAL_UART_Transmit(&huart2, rx_buf, Size, 100);
+//     HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf, RX_BUF_SIZE);
+//   }
+// }
+// void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
+// {
+//   if(huart->Instance==USART2){
 
-    HAL_UART_DMAStop(&huart2);
-    memset(rx_buf,0,Size);
+//     HAL_UART_Transmit(&huart2, rx_buf, Size, 100);
 
-  }
-  HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf, RX_BUF_SIZE);
+//     HAL_UART_DMAStop(&huart2);
+//     memset(rx_buf,0,Size);
 
-}
+//   }
+//   HAL_UARTEx_ReceiveToIdle_DMA(&huart2, rx_buf, RX_BUF_SIZE);
 
+// }
+
+<<<<<<< Updated upstream
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart){
   HAL_UART_Receive_DMA(&huart2, rx_buf, RX_BUF_SIZE);
 }
 
+=======
+// void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart){
+//   HAL_UART_Receive_DMA(&huart2, rx_buf, RX_BUF_SIZE);
+// }
+>>>>>>> Stashed changes
--- operate-stm32/MyApp/njw_driver/myBt.c
+++ operate-stm32/MyApp/njw_driver/myBt.c
@@ -1,4 +1,19 @@
 #include "myBt.h"
+#include "usart.h"
+#include "stdio.h"
+
+// bt 모듈 처음 세팅
+void bt_Reset(){
+    bt_AtIsOk();
+    bt_SetName();
+    bt_SetRole();
+    bt_SetPassword();
+    bt_SetRole();
+    bt_SetOnlyTarget();
+    bt_SearchType();
+    bt_CanConnect();
+    bt_SearchSlave();
+}
 
 // HC-05 AT 확인 코드
 void bt_AtIsOk(void){
@@ -116,9 +131,9 @@ void bt_SetRole(void){
 
 }
 
-// HC-05 AT set master
-void bt_SetTarget(void){
-    uint8_t tx[] = "AT+CMODE=0\r\n";
+// HC-05 AT only target slave
+void bt_SetOnlyTarget(void){
+    uint8_t tx[] = "AT+CMODE=1\r\n";
     uint8_t rx[16] = {0};
 
     HAL_StatusTypeDef tx_status;
@@ -145,8 +160,37 @@ void bt_SetTarget(void){
 
 }
 
-// HC-05 SPP Init
-void bt_SPPInit(void){
+// HC-05 SearchType
+void bt_SearchType(void){
+    uint8_t tx[] = "AT+INQM=0,5,9\r\n";
+    uint8_t rx[16] = {0};
+
+    HAL_StatusTypeDef tx_status;
+    HAL_StatusTypeDef rx_status;
+
+    tx_status = HAL_UART_Transmit(&huart1,
+                                  tx,
+                                  sizeof(tx) - 1,
+                                  1000);
+
+    rx_status = HAL_UART_Receive(&huart1,
+                                 rx,
+                                 4,
+                                 2000);
+
+    printf("TX status = %d\r\n", tx_status);
+    printf("RX status = %d\r\n", rx_status);
+
+    if (rx_status == HAL_OK)
+    {
+        printf("RX = %c%c%c%c\r\n",
+               rx[0], rx[1], rx[2], rx[3]);
+    }
+
+}
+
+// HC-05 SPP Init - bt can connect?
+void bt_CanConnect(void){
     uint8_t tx[] = "AT+INIT\r\n";
     uint8_t rx[16] = {0};
 
@@ -175,9 +219,9 @@ void bt_SPPInit(void){
 }
 
 
-// HC-05 SPP Init
-void bt_SPPInit(void){
-    uint8_t tx[] = "AT+INIT\r\n";
+// HC-05 SearchSlave
+void bt_SearchSlave(void){
+    uint8_t tx[] = "AT+INQ\r\n";
     uint8_t rx[16] = {0};
 
     HAL_StatusTypeDef tx_status;
